2b7b2f07efaf52c47b0d9a278254d539491bf78f
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-05 04:43:02 +0100

    added installed brew packages to brew_packages.txt

--- fish/config.fish
+++ fish/config.fish
@@ -21,3 +21,13 @@ function next
   echo $command
   eval $command
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/gerardhernandez/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH

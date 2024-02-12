dfc603b05f0f27f3959c91de2222f0c74ac10634
Gerard Hernandez <ger.almenara@gmail.com>
2024-02-12 03:52:09 +0100

    Add new brew packages and installation script

---
+++ install.ts
@@ -0,0 +1,18 @@
+import { $ } from "bun"
+
+const brewFile = Bun.file("brew_packages.txt")
+const packages = (await brewFile.text()).split("\n")
+
+const total = packages.length
+let count: number = 0;
+for (const pack of packages) {
+    console.log(`Installing ${pack}...`)
+    const startTime = Date.now()
+    const result = await $`brew install ${pack}`.quiet();
+    if (result.exitCode === 0) {
+        console.log(`Successfully installed ${pack} in ${(Date.now() - startTime) / 1000} seconds ${count}/${total}`)
+    } else {
+        console.error(`Error installing ${pack}`)
+    }
+    count++;
+}
